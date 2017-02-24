c31c3496e8cb3bc092c4ea65b17da6b5ad1c628f
Aiden Vaines <aiden@vaines.org>
2017-02-24 16:19:21 +0000

    inactive license fix
    Fixing issue where feature being activated belongs to a license which isn't enabled

--- Microsoft/Set-Office365License/Modules/Get-O365Licenses.ps1
+++ Microsoft/Set-Office365License/Modules/Get-O365Licenses.ps1
@@ -1,157 +1,7 @@
-﻿
-### Get-LicencedFeatures ###
+### Enable-O365Feature ###
 <#
 .SYNOPSIS
-    Collects all available services and license information for a given Office365 tenant
- 
-.DESCRIPTION
-    Collects all available services and license information for a given Office365 tenant. Stores the information in a JSON file named "O365LicenseStructure.json" in the execution directory.
-
-    JSON structure looks like this:
-    {
-    "Tenant":  [
-                   "XYZ1"
-               ],
-    "Licenses":  [
-                    {
-                         "ENTERPRISEPACK":  [
-                                                {
-                                                    "ActiveLicenses":  100,
-                                                    "AssignedLicenses":  13,
-                                                    "LockedOutLicenses":  0,
-                                                    "SuspendedLicenses":  0,
-                                                    "Features":  [
-                                                                "FLOW_O365_P2",
-                                                                "POWERAPPS_O365_P2",
-                                                                "TEAMS1",
-                                                                "PROJECTWORKMANAGEMENT",
-                                                                "SWAY",
-                                                                "INTUNE_O365",
-                                                                "YAMMER_ENTERPRISE",
-                                                                "RMS_S_ENTERPRISE",
-                                                                "OFFICESUBSCRIPTION",
-                                                                "MCOSTANDARD",
-                                                                "SHAREPOINTWAC",
-                                                                "SHAREPOINTENTERPRISE",
-                                                                "EXCHANGE_S_ENTERPRISE"   
-                                                                ]
-                                                    }
-                                                ]
-                        }
-                 ]
-    }
-  
-.PARAMETER tenant
-    Mandatory. Name of Office 365 Tenant. "(Get-MsolAccountSku).accountname" will give you this info
-  
-.OUTPUTS
-    O365LicenseStructure.json
- 
-.NOTES
- 
-.EXAMPLE
-    Get-LicencedFeatures -tenant "XYZ1"
-#>
- Function Get-LicencedFeatures { 
-[CmdletBinding()]
-    Param (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$Tenant
-    )
-
-    Begin {
-        Log-write -logpath $Script:LogPath -linevalue "`t`t`tWorking with tenant: $Tenant"
-        $365LicesesContainer = new-object -TypeName PSObject
-        $SKUFeatureDetails = @()
-        $365ReportContainer = new-object -TypeName PSObject
-        $MapFileName = ".\Output\" + $Tenant + "O36LicenseStructure.json"
-    
-   }
-
-    Process{
-
-        try{ 
-    
-            #Add tenant details to the container
-            $365ReportContainer | 
-                Add-Member -MemberType NoteProperty -Name "Tenant" -Value $tenant
-            
-            Log-write -logpath $Script:LogPath -linevalue "`t`tTenant details collected"
-
-
-            #Collect all Licenses currently available
-            Log-write -logpath $Script:LogPath -linevalue "`t`tLooking for all available licenses & features"
-            
-            foreach ($SKULicense in Get-MsolAccountSku){
-                #Collect the SKU name as a string for logging messages etc. like "ENTERPRISEPACK"
-                $SKULicenseName = $SKULicense.AccountSkuId 
-                 
-                                
-                #Look for each feature/service plan in the current license
-                foreach ($SKUFeature in $SKULicense.servicestatus){
-                    $SKUFeatureName = $SkuFeature.ServicePlan.ServiceName
-                    
-                    #Record the service plan/feature name          
-                    $SKUFeatureDetails += $SKUFeatureName
-
-                }#end foreach (feature)
-
-
-                #Create an object for each individual license like "ENTERPRISEPACK"
-                $O365LicenseDetails = new-object -TypeName PSObject | 
-                    Add-Member -Force -PassThru NoteProperty -Name "ActiveLicenses" -Value $SKULicense.ActiveUnits |
-                    Add-Member -Force -PassThru NoteProperty -Name "AssignedLicenses" -Value $SKULicense.ConsumedUnits |
-                    Add-Member -Force -PassThru NoteProperty -Name "LockedOutLicenses" -Value $SKULicense.LockedoutUnits |
-                    Add-Member -Force -PassThru NoteProperty -Name "SuspendedLicenses" -Value $SKULicense.SuspendedUnits |
-                    Add-Member -Force -PassThru NoteProperty -Name "Features" -Value @($SKUFeatureDetails)
-
-
-                #Add License SKU details to the container    
-                $365LicesesContainer | 
-                    Add-Member -MemberType NoteProperty -Name $SKULicenseName -Value @($O365LicenseDetails)
-                
-                  
-                #Reset the features array ready for the next license
-                $SKUFeatureDetails = @()  
-
-                #Spammy
-                #Log-write -logpath $Script:LogPath -linevalue "`t`tLicense details collected: $SKULicenseName"              
-                
-            }#end foreach (license)
-
-
-            
-            $365ReportContainer | 
-                Add-Member -MemberType NoteProperty -Name "Licenses" -Value @($365LicesesContainer) 
-                Log-write -logpath $Script:LogPath -linevalue "`t`t`tLicense details collected"
-
-          
-            try{
-                #Write the JSON to file
-               
-                $365ReportContainer| convertto-json -depth 10 | out-file $MapFileName
-                Log-write -logpath $Script:LogPath -linevalue "`t`tJSON Written to file successfully"
-                
-                } catch {
-                
-                    Log-Error -LogPath $Script:LogPath -ErrorDesc $_.Exception -ExitGracefully $False
-                
-                }
-
-
-        }catch{
-            Log-Error -LogPath $Script:LogPath -ErrorDesc $_.Exception -ExitGracefully $False
-        }#Try/Catch
-    }#Process
-}#Function
-
-
-
-
-
-### Get-O365LicenseStructure ###
-<#
-.SYNOPSIS
-    Loads the license map created by Get-LicencedFeatures in to the script namespace
+    Enables a given office 365 feature
  
 .DESCRIPTION
     Loads the license map created by Get-LicencedFeatures in to the script namespace
@@ -160,32 +10,111 @@
     $script:O365LicenseMap is available
  
 .NOTES
-    
+    uses the JSON file generated by the Get-O365Licenses script
 
+.PARAMETER Tenant
+    Mandatory. Name of the O365 tenant
+
+.PARAMETER License
+    Mandatory. Name of the O365 license
+
+.PARAMETER Feature
+    Mandatory. Name of the O365 feature
+
+.PARAMETER UPN
+    Mandatory. UPN for the user
+    
 .EXAMPLE
-    Get-O365LicenseMap
+    Enable-O365Feature -Tenant "MYTENANT1" -License "EMS" -Feature "INTUNE_A" -UPN "User.Name@domain.com"
 
 #>
- Function Get-O365LicenseMap { 
+ Function Enable-O365Feature { 
  [CmdletBinding()]
     Param (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$Tenant
+
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$Tenant,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$License,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$Feature,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$UPN
+
     )
+
     Begin{
-    
-    $MapFileName = ".\Output\" + $Tenant + "O36LicenseStructure.json"
-    
+
+         $ProductCode = $Tenant + ":" + $License
+
+         Log-write -logpath $Script:LogPath -linevalue "`t`t`tEnabling $Feature for $UPN"
+         
+         #Create an array of the ServicePlans currently disabled that are not "$ProductCode". 
+         #The SetMSOUserLicense function does not contain an enable argument, only disable so this is backwards
+         $DisabledPlans = @()
+
     }
     
     Process {
 
         try{ 
-  
-            Get-LicencedFeatures -Tenant $Tenant
+           #Get the O365 user object associated with the UPN
+           $msoluser = get-msolUser -UserPrincipalName $UPN
 
-            $script:O365LicenseMap = convertfrom-json -InputObject (Get-Content $MapFileName -raw)
+           #Does the user have an activated license for this?
+           If ($msoluser.licenses.accountSKU.SkuPartNumber -contains $License){
+                #User is alredy licensed for this SKU 
+           }else{
+	            #user doesnt have a license for this SKU
+	            #Enable the SKU
+	            Log-write -logpath $Script:LogPath -linevalue "`t`t`t`t$UPN isn't enabled with a license for $License, activating..."
+	            Set-MsolUserLicense -UserPrincipalName $UPN -AddLicenses $ProductCode
+	
+	            #Disable all the features in it based on the JSON map generated
+	            Log-write -logpath $Script:LogPath -linevalue "`t`t`t`tDisabling all features"
+	            $BaseLicenseOptions = New-MsolLicenseOptions -AccountSkuId $ProductCode -DisabledPlans $O365LicenseMap.Licenses.$ProductCode.Features
 
-            Log-write -logpath $Script:LogPath -linevalue "`t`tLicense Map loaded"
+	            Set-MsolUserLicense -UserPrincipalName $UPN -LicenseOptions $BaseLicenseOptions
+                
+                #Get the MMSLUser again
+                $msoluser = get-msolUser -UserPrincipalName $UPN
+           }
+
+           
+           #Get the object for the SKU requested
+           $msolSku = $msoluser.Licenses | Where-Object {$_.AccountSKUID -eq $ProductCode}
+
+           #Get the services which are currently disabled within the SKU
+           $msolservices = $msolSku.ServiceStatus | Where-Object {$_.ServicePlan.Servicename -ne $Feature -and $_.ProvisioningStatus -eq "Disabled"}
+
+           Log-write -logpath $Script:LogPath -linevalue "`t`t`tEnumerating existing licensed features"
+           foreach ($msolservice in $msolservices){
+               $DisabledPlans += $msolservice.ServicePlan.ServiceName
+           }
+
+
+           #Create a new object for the plans to enable
+           $NewLicenseOptions = New-MsolLicenseOptions -AccountSkuId $ProductCode -DisabledPlans $DisabledPlans
+           Log-write -logpath $Script:LogPath -linevalue "`t`t`tApplying new license settings"
+           Set-MsolUserLicense -UserPrincipalName $UPN -LicenseOptions $NewLicenseOptions
+           
+                       
+            #Check to see if it worked
+            Log-write -logpath $Script:LogPath -linevalue "`t`tChecking new license settings"
+            
+            #Update the msolSKU object now the feature should have been enabled
+            $msolSku = $msoluser.Licenses | Where-Object {$_.AccountSKUID -eq $ProductCode}
+            
+            #Check to see if the requested feature is still disabled
+            if ($msolSku.ServiceStatus | Where-Object {$_.ServicePlan.Servicename -eq $Feature -and $_.ProvisioningStatus -eq "Disabled"}){
+                
+                #Feature is still disabled
+                Log-Error -LogPath $Script:LogPath -ErrorDesc "[ERROR]$_.Exception" -ExitGracefully $False
+
+
+            } else {
+                #Feature is not disabled
+                Log-write -logpath $Script:LogPath -linevalue "`t`t`t$Feature has been enabled"
+
+            }
+
+            Log-write -logpath $Script:LogPath -linevalue "`t`tFeature Licensed Successfully"
             
         }catch{
 
@@ -196,56 +125,3 @@
     }#end Process
 
 }#Function
-
-
-
-
-### Get-O365LicenseAvailable ###
-<#
-.SYNOPSIS
-    Based on the Get-LicencedFeatures JSON map, is there atleast 1 available license
- 
-.DESCRIPTION
-    Based on the Get-LicencedFeatures JSON map, is there atleast 1 available license
-
-.OUTPUTS
-    True/False
- 
-.NOTES
-    
-
-.EXAMPLE
-    if (Get-O365LicenseAvailable -Tenant $Tenant -License $License){
-        #is true
-        #Licenses are available
-    }
-
-#>
- Function Get-O365LicenseAvailable { 
- [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$Tenant,
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$License
-    )
-
-    try{ 
-        $ProductCode = $Tenant + ":" + $license       
-        
-        If ($script:O365LicenseMap.Licenses.$ProductCode.ActiveLicenses - $script:O365LicenseMap.Licenses.$ProductCode.AssignedLicenses -ge 1){
-            #active lisences - assigned licenses is 1 or greater
-            return $true
-
-        } else{
-
-            return $false
-        }
-
-    }catch{
-
-        Log-Error -LogPath $Script:LogPath -ErrorDesc $_.Exception -ExitGracefully $False
-        return $false
-    
-    }#Try/Catch
-
-}#Function
-
