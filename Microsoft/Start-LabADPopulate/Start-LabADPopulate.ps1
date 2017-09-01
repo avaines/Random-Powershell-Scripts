6b3f9ca36e1d32a9b5124933a0af2be9f0fa7131
Aiden Vaines <aiden@vaines.org>
2017-09-01 10:19:30 +0100

    Adding Start-LabADPopulate script
    A script to populate a lab environment with pretend users

---
+++ Microsoft/Start-LabADPopulate/Start-LabADPopulate.ps1
@@ -0,0 +1,127 @@
+﻿#
+# Global variables
+#
+# User properties
+# Base OU for users, sub OU for country code will be created
+$BaseOU = "OU=Users,OU=Company,DC=lab,DC=local" 
+
+# Domain Details
+$orgShortName = "LAB"                         # This is used to build a user's sAMAccountName
+$dnsDomain = "lab.local"                      # Domain is used for e-mail address and UPN
+
+#Companies the user could be part of
+$Companies = @("Lab Corp", "Lab Ltd", "Env. Testing Industries", "MyLab Inc.")                      
+
+#Departments and their sub positions users could be part of
+$Departments = (                             # Departments and associated job titles to assign to the users
+                  @{"Name" = "Finance & Accounting"; Positions = ("Manager", "Accountant", "Data Entry")},
+                  @{"Name" = "Human Resources"; Positions = ("Manager", "Administrator", "Officer", "Coordinator")},
+                  @{"Name" = "Sales"; Positions = ("Manager", "Representative", "Consultant")},
+                  @{"Name" = "Marketing"; Positions = ("Manager", "Coordinator", "Assistant", "Specialist")},
+                  @{"Name" = "Engineering"; Positions = ("Manager", "Engineer", "Scientist")},
+                  @{"Name" = "Consulting"; Positions = ("Manager", "Consultant")},
+                  @{"Name" = "IT"; Positions = ("Manager", "Engineer", "Technician")}
+               )
+$employeeTypes = @("EMP", "Regular", "Contractor", "Fixed Term Regular", "Temporary", "Full-Time")
+
+
+#File with list of users format as:
+#   Title	GivenName	Surname	EmailAddress	    Username	Password	TelephoneNumber	StreetAddress	    City	ZipCode	Country	CountryFull
+#   Mr.	    Mike	    Lowe	MikeLowe@rhyta.com	10073001	Eigei7ye	0951 85 76 34	Mühlenstrasse 30	Bamberg	96010	DE	    Germany
+$UsersFile = ".\FakeUserData.csv"
+
+
+#################################
+#             Begin
+#################################
+
+$ListOfUsers = import-csv $UsersFile
+
+foreach($User in $Listofusers){
+    if((dsquery user -samid $user.Username) -eq $null){
+            Write-host "Creating user" $User.Givenname $User.Surname
+            #Assigning the user to a company
+            $UserCompany = $Companies[(get-random -Minimum 0 -max $Companies.count)]
+
+            #Assign the user to a department
+            $DepartmentIndex = get-random -Minimum 0 -max $departments.count
+            $UserDepartment = $Departments[$DepartmentIndex].name
+            $UserJob = $Departments[$DepartmentIndex].Positions[$(Get-Random -Minimum 0 -Maximum $Departments[$DepartmentIndex].Positions.Count)]       
+
+            $UserEmployeeType = $employeeTypes[(get-random -Minimum 0 -max $employeeTypes.count)]
+            Write-host "`tAssigning the user to $UserJob in the $UserDapartment deptartment for $UserCompany, their employment type is $UserEmplyeeType"
+
+
+            $UserFullName = $User.GivenName + " " + $User.Surname
+            $UserPrincipalName = $User.Username + "@lab.local"
+
+            #Check OU Exists, create it if not
+            $UserOUPath = "OU=" + $User.Country + "," + $BaseOU
+            If (!([adsi]::Exists("LDAP://" + $UserOUPath))){
+                try {
+                    New-ADOrganizationalUnit $User.Country -Path $BaseOU
+                }catch{
+                    $UserOUPath = $BaseOU
+                }
+            }
+      
+            #Conver the plain text password to a secure string
+            $UserPassword = ConvertTo-SecureString -AsPlainText $user.Password -Force
+    
+            write-host "`t`t`tCreating AD Object...`n"
+
+            New-AdUser -SamAccountName $User.Username -Name $UserFullName -Path $UserOUPath -AccountPassword $UserPassword -Enabled $True `
+            -GivenName $User.GivenName -Surname $User.Surname -DisplayName $UserFullName -EmailAddress $User.EmailAddress`
+            -StreetAddress $User.StreetAddress -City $User.City -PostalCode $User.ZipCode -Country $User.Country -UserPrincipalName $UserPrincipalName `
+            -Company $UserCompany -Department $UserDepartment -EmployeeNumber $User.Username -Title $UserJob -OfficePhone $User.TelephoneNumber
+    
+     }else{
+
+        write-host $User.GivenName $User.Surname " already exists"
+
+     }
+     
+}
+
+
+ write-host "All users created. Calculating managers..."
+
+$AllUsers = get-adUser -searchbase $BaseOU -filter * -properties Title,Company,Department,Manager
+
+# Foreach company
+foreach ($MGRCompany in $Companies){
+    write-host "`tWorking with $MGRCompany"
+
+    #Get the list of all users in this company
+    $AllUsers_Company = $Allusers | where-object {$_.Company -eq $MGRCompany}
+    
+
+    # Foreach department in the company...
+    foreach($MGRDepartment in $Departments.name){
+        write-host "`t`Setting the managers for $MGRCompany, $MGRDepartment" 
+
+        #Get the list of departments in this company...
+        $AllUsers_Company_Dept = $AllUsers_Company | where-object {$_.Department -eq $MGRDepartment}
+
+        #Get the list of managers in this department
+        $MGRDepartment_Managers = $AllUsers_Company_Dept | Where-object{$_.title -match "Manager"}
+
+        
+        #for each user who is not a manger in this department...
+        foreach($NotManagerUSR in ($AllUsers_Company_Dept | Where-object{$_.title -notmatch "Manager" -and $_.manager -eq $null})){
+            
+            # Get a random manager from this department
+            $UserMGR = $MGRDepartment_Managers[(get-random -Minimum 0 -max $MGRDepartment_Managers.count)]
+
+            write-host "`t`t`tAssigning " $UserMGR.name " as the manager for " $NotManagerUSR.name
+
+            #Set the Non-manager's manager to the randomly assigned manager
+            Set-aduser $NotManagerUSR.SamAccountName -Manager $UserMGR.SamAccountName
+
+        }#End Manager
+    }#End foreach department
+}#End foreach Company
+
+
+pause
+Exit
---
+++ Microsoft/Start-LabADPopulate/readme.md
@@ -0,0 +1,87 @@
+Start-LabADPopulate
+===================
+A script to populate a lab environment with pretend users, 
+
+----------
+
+ 1. Copy this script folder to your lab DC
+ 2. Update the variables at the top of the "Start-LabADPopulate.ps1"script
+ 3. Create the base OU structure (see below)
+ 3. Execute the "Start-LabADPopulate.ps1 script
+ 4. Wait
+
+
+----------
+
+How It Works
+---------
+When you need to test Active Directory in a lab with sample users, creating sufficiently realistic test accounts is a time consuming and tedious process. There are a few quick scripts for creating something similar but many of them only create basic users which don't emulate a production environment very well.
+
+
+This script will create users with the following attributes:
+
+ - **SAMAccount Name** - A unique ID number authenticate a user
+ - **Name** - A user has a full name sourced from a list of regional names
+ - **Address** - People need an address, again regionally sourced addresses from Germany, Spain, Italy, France, Poland and the United Kingdom
+ - **Email Address**
+ - **Company** - Most sufficiently large organisations have multiple internal companies, this script accepts a list of possible companies and assigns each user a random one
+ - **Department** - Large organisations have many departments, this script accepts a list of possible departments and assigns each user a random one
+ - **Job Title** - Similar to departments, each department often has multiple roles, this script records what job roles exist in each department and assigns a random one to the user
+ - **Manager** - People have managers. After creating the user accounts, the script will assign each user a manager from their department and company *(A "Manager" does have to be a valid job role in the department the user is assigned to for this to work)*
+ - **EmployeeNumber** - Some organisations assign an ID number to users, this is especially helpful when people have non-alphanumeric characters in their name
+ 
+
+Setup
+---------
+You will need to prepare the environment by creating the following OU structure first:
+
+      LAB.local
+       \---Company
+           \---Users
+
+Update the first 30 or so lines of the script to match your lab environment
+
+ 1. This is the OU in your lab where the user accounts will reside
+
+          $BaseOU = "OU=Users,OU=Company,DC=lab,DC=local"
+    
+
+ 2. The organisation short name or NETBIOS name:
+
+          $orgShortName = "LAB"
+
+ 3. A list of companies within the organisation  a user could belong to.
+    Some organisations have smaller companies within, this helps model
+    that structure.
+    
+          $Companies = @("Lab Corp", "Lab Ltd", "Env. Testing Industries", "MyLab Inc.")
+
+ 4. A list of departments, each with a set of job roles. Each user will be assigned a random department and a role/position from within it. Once the script has finished creating the users, it will run back through all the users it created and assign everyone in each department a manager from the same department and company.
+
+          $Departments = (
+             @{"Name" = "Finance & Accounting"; Positions = ("Manager", "Accountant", "Data Entry")},
+             @{"Name" = "Human Resources"; Positions = ("Manager", "Administrator", "Officer", "Coordinator")},
+             @{"Name" = "Sales"; Positions = ("Manager", "Representative", "Consultant")},
+             @{"Name" = "Marketing"; Positions = ("Manager", "Coordinator", "Assistant", "Specialist")},
+             @{"Name" = "Engineering"; Positions = ("Manager", "Engineer", "Scientist")},
+             @{"Name" = "Consulting"; Positions = ("Manager", "Consultant")},
+             @{"Name" = "IT"; Positions = ("Manager", "Engineer", "Technician")}
+           )
+     
+ 5. Finally, the path for the user data, as long as the fields are the
+    same feel free to replace/update it.
+
+          $UsersFile = ".\FakeUserData.csv"
+
+
+Credit
+---------
+The information stored in "FakeUserData.csv" was provided by https://www.fakenamegenerator.com/ and contains around 600 random users doted around Europe in the following countries:
+ - Germany
+ - Spain
+ - Italy
+ - France
+ - United Kingdom
+ - Poland
+
+The foundation for this project is based on work by Helge Klein's blog post here: https://helgeklein.com/blog/2015/02/creating-realistic-test-user-accounts-active-directory/
